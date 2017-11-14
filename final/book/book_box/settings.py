8cbdfa46fa661752487182949427441e0213c189
Viwe Lolwane <viwelolwane@gmail.com>
2017-11-14 09:10:52 +0000

    10th build

--- final/book/book_box/settings.py
+++ final/book/book_box/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     # ************************************************************************
     'rest_framework',  # REST API
 
+    'star_ratings',  # Ratings app
     'jquery',  # jQuery integration app
     'djangoformsetjs',  # JavaScript for FormSets
 
